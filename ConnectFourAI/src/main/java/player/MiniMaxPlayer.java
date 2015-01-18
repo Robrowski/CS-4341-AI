9cb742a9d38877c8d10028c7677357168fea4348
mscosti <mscosti3@gmail.com>
2015-01-18 14:52:29 -0500

    refactored and added comments to minimax player

--- ConnectFourAI/src/main/java/player/MiniMaxPlayer.java
+++ ConnectFourAI/src/main/java/player/MiniMaxPlayer.java
@@ -8,6 +8,7 @@ import common.MoveHolder;
 
 public class MiniMaxPlayer extends AbstractPlayer{
 
+	/** The maximum depth we will allow for mini max*/
 	int MAXDEPTH = 2;
 	
 	public MiniMaxPlayer(String[] args) {
@@ -28,78 +29,100 @@ public class MiniMaxPlayer extends AbstractPlayer{
 		return next.getCol();
 	}
 	
+	/**
+	 * An implementation of the miniMax algorithm for determining the 
+	 * current best possible move for the player by minimizing the opponent's
+	 * chance of winning while increasing the player's chance.  
+	 * 
+	 * Currently Recurses through every possibility of moves, 
+	 * up to a certain depth.
+	 * @param parentMove The previous move made at a parent state
+	 * @param current The current board state we are looking at
+	 * @param depth the current depth of the tree we are at
+	 * @param player The player whose turn we are evaluating. If it is us, we
+	 * 					are maximizing, if opponent we are minimizing.
+	 * 
+	 * @return The decided best move
+	 */
 	private MoveHolder miniMax(int parentMove, Board current, int depth, int player){
-		if (depth == MAXDEPTH){
+		List<Integer> moves = current.getPossibleMoves();
+		
+		/** If depth limit reached or If no possible moves, 
+		 * we can procede to estimate the current board's value */
+		if (depth == MAXDEPTH || moves.size() == 0){
 			int estimate = estimateBoard(current);
 			MoveHolder moveEval = new MoveHolder(parentMove,estimate);
 			return moveEval;
 		}
 		else{
-			List<Integer> moves = current.getPossibleMoves();
-			
-			// If no possible moves, we are at a leaf in our move
-			// tree and can procede to estimate its value
-			if (moves.size() == 0) { 
-				int estimate = estimateBoard(current);
-				MoveHolder moveEval = new MoveHolder(parentMove,estimate);
-				return moveEval;
+			if (player == this.playerNumber){ // maximizing score
+				
+				int bestValue = 0; // init to - infinity
+				MoveHolder bestMove = new MoveHolder();
+				for (int move : moves){
+					/** copy the current board in order to split and
+					 * add a new board state to the tree. */
+					Board newBoard = new Board(current);
+					newBoard.addPiece(move, player);
+					
+					/** Run miniMax on the next layer of the tree, which
+					 * is to maximize the move of the opponent. */
+					MoveHolder minMaxMove = miniMax(move,newBoard,
+											depth++,this.opponentNum);
+					
+					if (minMaxMove.getValue() > bestMove.getValue()){
+						/** 
+						 * set the best move value to the better move
+						 * we just found. Set the Column to the move
+						 * that leads to this value before recursing to 
+						 * eventually use the first split, NOT the 
+						 * last move in the tree.
+						 */
+						bestMove.setValue(minMaxMove.getValue());
+						bestMove.setCol(move);
+					}
+				}
+				return bestMove;
 			}
-			else{
-				if (player == this.playerNumber){ // maximizing
-					int bestValue = 0; // init to - infinity
-					MoveHolder bestMove = new MoveHolder();
-					for (int move : moves){
-						// copy the current board in order to split and
-						// add a new board state to the tree.
-						Board newBoard = new Board(current);
-						newBoard.addPiece(move, player);
-						
-						// Run miniMax on the next layer of the tree, which
-						// is to minimize the move of the opponent.
-						MoveHolder minMaxMove = miniMax(move,newBoard,
-												depth++,this.opponentNum);
-						
-						if (minMaxMove.getValue() > bestMove.getValue()){
-							// set the best move value to the better move
-							// we just found. Set the Column to the move
-							// that leads to this value before recursing to 
-							// eventually use the first split, NOT the 
-							// last move in the tree.
-							bestMove.setValue(minMaxMove.getValue());
-							bestMove.setCol(move);
-						}
+			else { // minimizing score
+				
+				int bestValue = 0; // init to + infinity
+				MoveHolder bestMove = new MoveHolder();
+				for (int move : moves){
+					/** copy the current board in order to split and
+					 * add a new board state to the tree. */
+					Board newBoard = new Board(current);
+					newBoard.addPiece(move, player);
+					
+					/** Run miniMax on the next layer of the tree, which
+					 * is to maximize the move of the opponent. */
+					MoveHolder minMaxMove = miniMax(move,newBoard,
+											depth++,this.playerNumber);
+					if (minMaxMove.getValue() < bestMove.getValue()){
+						/** 
+						 * set the best move value to the better move
+						 * we just found. Set the Column to the move
+						 * that leads to this value before recursing to 
+						 * eventually use the first split, NOT the 
+						 * last move in the tree.
+						 */
+						bestMove.setValue(minMaxMove.getValue());
+						bestMove.setCol(move);
 					}
-					return bestMove;
 				}
-				else { // minimizing
-					int bestValue = 0; // init to + infinity
-					MoveHolder bestMove = new MoveHolder();
-					for (int move : moves){
-						// copy the current board in order to split and
-						// add a new board state to the tree.
-						Board newBoard = new Board(current);
-						newBoard.addPiece(move, player);
-						
-						// Run miniMax on the next layer of the tree, which
-						// is to maximize the move of the opponent.
-						MoveHolder minMaxMove = miniMax(move,newBoard,
-												depth++,this.playerNumber);
-						if (minMaxMove.getValue() < bestMove.getValue()){
-							// set the best move value to the better move
-							// we just found. Set the Column to the move
-							// that leads to this value before recursing to 
-							// eventually use the first split, NOT the 
-							// last move in the tree.
-							bestMove.setValue(minMaxMove.getValue());
-							bestMove.setCol(move);
-						}
-					}
-					return bestMove;
-				}
+				return bestMove;
 			}
 		}
 	}
 	
+	/**
+	 * The method that evaluates a given board, estimating how good of
+	 * a board state it is
+	 * 
+	 * TODO: Implement this
+	 * @param current The board to estimate
+	 * @return
+	 */
 	private int estimateBoard(Board current){
 		return 0;
 	}
