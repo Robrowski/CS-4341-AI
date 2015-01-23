1d5a1ef032103268a229ef8394dfc48fe034e573
mscosti <mscosti3@gmail.com>
2015-01-23 16:43:25 -0500

    added tests to confirm an entire scoreboard update works corectly when there are pieces on the board

--- ConnectFourAI/src/test/java/common/ScoreBoardTest.java
+++ ConnectFourAI/src/test/java/common/ScoreBoardTest.java
@@ -46,6 +46,34 @@ public class ScoreBoardTest {
 
 		assertTrue(boardEquals(expectedScore, board.getPlayerScoreBoard()));
 	}
+	
+	@Test
+	public void test_initScore_4x4_connect3() {
+		ScoreBoard board = new ScoreBoard(4, 4, 3);
+
+		int[][] expectedScore = { 
+				{ 3, 4, 4, 3 }, 
+				{ 4, 7, 7, 4 },
+				{ 4, 7, 7, 4 }, 
+				{ 3, 4, 4, 3 } };
+
+		System.out.println("4x4 connect3");
+		printBoard(board, "player");
+
+		assertTrue(boardEquals(expectedScore, board.getPlayerScoreBoard()));
+	}
+	
+	@Test
+	public void test_initScore_3x2_connect2() {
+		ScoreBoard board = new ScoreBoard(3, 2, 2);
+
+		int[][] expectedScore = { { 3, 5, 3 }, { 3, 5, 3 } };
+
+		System.out.println("3x3 connect2");
+		printBoard(board, "player");
+
+		assertTrue(boardEquals(expectedScore, board.getPlayerScoreBoard()));
+	}
 
 	@Test
 	public void test_initScore_7x1_connect4() {
@@ -129,6 +157,153 @@ public class ScoreBoardTest {
 		printBoard(board, "player");
 
 		assertTrue(boardEquals(expectedScore, board.getPlayerScoreBoard()));
+	}
+
+	@Test
+	public void test_updatePlayerScoreBoard_3x3_connect2(){
+		ScoreBoard board = new ScoreBoard(3,3,2);
+		
+		int[][] opponentMoves_col1 =
+			{
+				{ 9, 9, 9 },
+				{ 9, 9, 9 },
+				{ 9, 2, 9 } };
+
+		board.setBoard(opponentMoves_col1);
+
+		int[][] expectedPlayerScore = { 
+				{ 3, 5, 3 }, 
+				{ 4, 7, 4 }, 
+				{ 2, 0, 2 } };
+		
+		int[][] expectedOpponentScore = {
+				{3, 5, 3},
+				{5, 8, 5},
+				{3, 5, 3}};
+
+		System.out.println("3x3 connect 2, opponent moves");
+		printBoard(board, "player");
+		assertTrue(boardEquals(expectedPlayerScore, board.getPlayerScoreBoard()));
+		assertTrue(boardEquals(expectedOpponentScore,
+				board.getOpponentScoreBoard()));
+
+	}
+
+	@Test
+	public void test_updateOpponentScoreBoard_3x3_connect2(){
+		
+		ScoreBoard board = new ScoreBoard(3, 3, 2);
+		
+		int[][] playerMoves_col0 ={
+				{ 9, 9, 9 },
+				{ 9, 9, 9 },
+				{ 1, 9, 9 } };
+
+		board.setBoard(playerMoves_col0);
+
+		int[][] expectedPlayerScore = { 
+				{ 3, 5, 3 }, 
+				{ 5, 8, 5},
+				{ 3, 5, 3}};
+
+		int[][] expectedOpponentScore = {
+				{3, 5, 3},
+				{ 4, 7, 5 }, 
+				{ 0, 4, 3 } };
+
+		System.out.println("3x3 connect 2, player moves");
+		printBoard(board, "opp");
+		assertTrue(boardEquals(expectedPlayerScore, board.getPlayerScoreBoard()));
+		assertTrue(boardEquals(expectedOpponentScore,
+				board.getOpponentScoreBoard()));
+		
+	}
+	
+	@Test
+	public void test_2moves_ScoreBoard_3x3_connect2() {
+		
+		ScoreBoard board = new ScoreBoard(3, 3, 2);
+		
+		int[][] moves = {
+				{ 9, 9, 9 },
+				{ 9, 9, 9 },
+				{ 1, 2, 9 } };
+
+		board.setBoard(moves);
+
+		int[][] expectedPlayerScore = { 
+				{ 3, 5, 3 }, 
+				{ 4, 7, 4 }, 
+				{ 2, 0, 2 } };
+
+		int[][] expectedOpponentScore = {
+				{3, 5, 3},
+				{ 4, 7, 5 }, 
+				{ 0, 4, 3 } };
+
+		System.out.println("3x3 connect 2, player moves");
+		printBoard(board, "opp");
+		assertTrue(boardEquals(expectedPlayerScore, board.getPlayerScoreBoard()));
+		assertTrue(boardEquals(expectedOpponentScore,
+				board.getOpponentScoreBoard()));
+		
+	}
+	
+
+	@Test
+	public void test_3moves_ScoreBoard_3x3_connect2() {
+		
+		ScoreBoard board = new ScoreBoard(3, 3, 2);
+		
+		int[][] moves = {
+				{ 9, 9, 9 },
+				{ 2, 9, 9 },
+				{ 1, 2, 9 } };
+
+		board.setBoard(moves);
+
+		int[][] expectedPlayerScore = { 
+				{ 2, 4, 3 }, 
+				{ 0, 6, 4 }, 
+				{ 1, 0, 2 } };
+
+		int[][] expectedOpponentScore = {
+				{3, 5, 3},
+				{ 4, 7, 5 }, 
+				{ 0, 4, 3 } };
+
+		System.out.println("3x3 connect 2, player moves");
+		printBoard(board, "opp");
+		assertTrue(boardEquals(expectedPlayerScore, board.getPlayerScoreBoard()));
+		assertTrue(boardEquals(expectedOpponentScore,
+				board.getOpponentScoreBoard()));
+		
+	}
+	
+	@Test
+	public void test_3moves_ScoreBoard_4x4_connect3() {
+
+		ScoreBoard board = new ScoreBoard(4, 4, 3);
+
+		int[][] moves = { 
+				{ 9, 9, 9, 9 }, 
+				{ 9, 9, 9, 9 },
+				{ 9, 9, 9, 9 },
+				{ 2, 2, 9, 2 } };
+
+		board.setBoard(moves);
+
+		int[][] expectedPlayerScore = { 
+				{ 3, 4, 4, 3 }, 
+				{ 3, 5, 6, 2 },
+				{ 3, 5, 5, 3 }, 
+				{ 0, 0, 2, 0 } };
+
+
+		System.out.println("4x4 connect 3, player moves");
+		printBoard(board, "player");
+		assertTrue(boardEquals(expectedPlayerScore, board.getPlayerScoreBoard()));
+
 	}
 
 	public boolean boardEquals(int[][] b1, int[][] b2) {
